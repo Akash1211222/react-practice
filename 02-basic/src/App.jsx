d158046c97c42c4abfb5316af3110a40e2130bff
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2024-01-20 13:44:01 +0530

    counter

--- 02-basic/src/App.jsx
+++ 02-basic/src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [add, setAdd] = useState(0);
+
+  const addValue = () => {
+    if (add < 50) {
+      setAdd(add + 1);
+    }
+  };
+
+  const removeValue = () => {
+    if (add > 0) {
+      setAdd(add - 1);
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Counter App</h1>
+      <h2>Counter value : {add}</h2>
+      <button onClick={addValue}>Add Value</button>
+      <br />
+      <button onClick={removeValue}>Remove Value</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
