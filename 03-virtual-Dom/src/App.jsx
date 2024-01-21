03a6e7290b53333573fa6c11d1d2dbebd3e79d07
Akash1211222 <90515043+Akash1211222@users.noreply.github.com>
2024-01-22 00:44:24 +0530

    currency convertor

--- 03-virtual-Dom/src/App.jsx
+++ 03-virtual-Dom/src/App.jsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import "./App.css";
 import ProductPage from "./pages/ProductPage.jsx";
 import PassGenPage from "./pages/PassGenPage.jsx";
-import CurrencyPage from "./pages/CurrencyPage.jsx";
+import CurrencyP from "./components/CurrencyP.jsx";
 
 function App() {
   return (
     <>
-      <CurrencyPage />
-      {/* <PassGenPage />
-      <ProductPage /> */}
+      <PassGenPage />
+      <CurrencyP />
+      <ProductPage />
     </>
   );
 }
--- 03-virtual-Dom/src/components/InputBox.jsx
+++ 03-virtual-Dom/src/components/InputBox.jsx
@@ -1,15 +1,14 @@
-import { useId } from "react";
+import React, { useId } from "react";
 
-function Input({
+function InputBox({
   label,
   amount,
   onAmountChange,
   onCurrencyChange,
-  currencyOption = [],
+  currencyOptions = [],
   selectCurrency = "usd",
   amountDisable = false,
   currencyDisable = false,
-
   className = "",
 }) {
   const amountInputId = useId();
@@ -38,12 +37,12 @@ function Input({
       <div className="w-1/2 flex flex-wrap justify-end text-right">
         <p className="text-black/40 mb-2 w-full">Currency Type</p>
         <select
-          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none "
+          className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
           value={selectCurrency}
           onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
           disabled={currencyDisable}
         >
-          {currencyOption.map((currency) => (
+          {currencyOptions.map((currency) => (
             <option key={currency} value={currency}>
               {currency}
             </option>
@@ -54,4 +53,4 @@ function Input({
   );
 }
 
-export default Input;
+export default InputBox;
